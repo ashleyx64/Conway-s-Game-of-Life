5f981833f602e3eb1c99d6dfb7306b5f5a611cb7
Ashley <Ashley@ASUS_SAPPHIRE>
2016-08-22 10:43:59 +0100

    Deleted unnecessary comments

--- src/gameoflife/GameController.java
+++ src/gameoflife/GameController.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gameoflife;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
-/**
- *
- * @author Ashley
- */
 public class GameController {
     private final int width, height, factor;
     private final Cell[][] gameMap;
--- src/gameoflife/GameOfLife.java
+++ src/gameoflife/GameOfLife.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gameoflife;
 
 import javafx.animation.AnimationTimer;
@@ -21,10 +16,6 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Ashley
- */
 public class GameOfLife extends Application {
     private boolean updateStates = false;
     private int refreshRate = 240;
